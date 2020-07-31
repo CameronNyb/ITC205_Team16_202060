84cc6b5f33562d7df5ebd32e7c87f3f9391e4896
CameronNyb <camnothin0@gmail.com>
2020-08-01 02:42:46 +1000

    Refactoring internal class variables

--- src/library/entities/Loan.java
+++ src/library/entities/Loan.java
@@ -6,44 +6,44 @@ import java.util.Date;
 @SuppressWarnings("serial")
 public class Loan implements Serializable {
 	
-	public static enum lOaN_sTaTe { CURRENT, OVER_DUE, DISCHARGED };
+	public static enum LoanState { CURRENT, OVER_DUE, DISCHARGED };
 	
-	private int LoAn_Id;
-	private Book BoOk;
-	private Member MeMbEr;
-	private Date DaTe;
-	private lOaN_sTaTe StAtE;
+	private int loanId;
+	private Book book;
+	private Member member;
+	private Date date;
+	private LoanState state;
 
 	
 	public Loan(int loanId, Book bOoK, Member mEmBeR, Date DuE_dAtE) {
-		this.LoAn_Id = loanId;
-		this.BoOk = bOoK;
-		this.MeMbEr = mEmBeR;
-		this.DaTe = DuE_dAtE;
-		this.StAtE = lOaN_sTaTe.CURRENT;
+		this.loanId = loanId;
+		this.book = bOoK;
+		this.member = mEmBeR;
+		this.date = DuE_dAtE;
+		this.state = LoanState.CURRENT;
 	}
 
 	
 	public void cHeCk_OvEr_DuE() {
-		if (StAtE == lOaN_sTaTe.CURRENT &&
-			Calendar.gEtInStAnCe().gEt_DaTe().after(DaTe)) 
-			this.StAtE = lOaN_sTaTe.OVER_DUE;			
+		if (state == LoanState.CURRENT &&
+			Calendar.gEtInStAnCe().gEt_DaTe().after(date)) 
+			this.state = LoanState.OVER_DUE;			
 		
 	}
 
 	
 	public boolean Is_OvEr_DuE() {
-		return StAtE == lOaN_sTaTe.OVER_DUE;
+		return state == LoanState.OVER_DUE;
 	}
 
 	
 	public Integer GeT_Id() {
-		return LoAn_Id;
+		return loanId;
 	}
 
 
 	public Date GeT_DuE_DaTe() {
-		return DaTe;
+		return date;
 	}
 	
 	
@@ -51,29 +51,29 @@ public class Loan implements Serializable {
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
 		StringBuilder sb = new StringBuilder();
-		sb.append("Loan:  ").append(LoAn_Id).append("\n")
-		  .append("  Borrower ").append(MeMbEr.GeT_ID()).append(" : ")
-		  .append(MeMbEr.GeT_LaSt_NaMe()).append(", ").append(MeMbEr.GeT_FiRsT_NaMe()).append("\n")
-		  .append("  Book ").append(BoOk.gEtId()).append(" : " )
-		  .append(BoOk.gEtTiTlE()).append("\n")
-		  .append("  DueDate: ").append(sdf.format(DaTe)).append("\n")
-		  .append("  State: ").append(StAtE);		
+		sb.append("Loan:  ").append(loanId).append("\n")
+		  .append("  Borrower ").append(member.GeT_ID()).append(" : ")
+		  .append(member.GeT_LaSt_NaMe()).append(", ").append(member.GeT_FiRsT_NaMe()).append("\n")
+		  .append("  Book ").append(book.gEtId()).append(" : " )
+		  .append(book.gEtTiTlE()).append("\n")
+		  .append("  DueDate: ").append(sdf.format(date)).append("\n")
+		  .append("  State: ").append(state);		
 		return sb.toString();
 	}
 
 
 	public Member GeT_MeMbEr() {
-		return MeMbEr;
+		return member;
 	}
 
 
 	public Book GeT_BoOk() {
-		return BoOk;
+		return book;
 	}
 
 
 	public void DiScHaRgE() {
-		StAtE = lOaN_sTaTe.DISCHARGED;		
+		state = LoanState.DISCHARGED;		
 	}
 
 }
