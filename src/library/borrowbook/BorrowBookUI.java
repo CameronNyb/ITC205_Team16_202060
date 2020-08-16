5119d990e8cd37f8b305b8249d66121f4d3cc10f
CameronNyb <camnothin0@gmail.com>
2020-08-16 22:36:07 +1000

    Spaces refactor

--- src/library/borrowbook/BorrowBookUI.java
+++ src/library/borrowbook/BorrowBookUI.java
@@ -3,42 +3,42 @@ import java.util.Scanner;
 
 
 public class BorrowBookUI {
-	
-	public static enum UI_STATE { INITIALISED, READY, RESTRICTED, SCANNING, IDENTIFIED, FINALISING, COMPLETED, CANCELLED };
 
-	private BorrowBookControl control;
-	private Scanner input;
-	private UI_STATE state;
-	
-	public BorrowBookUI(BorrowBookControl control) {
-		this.control = control;
-		input = new Scanner(System.in);
-		state = UI_STATE.INITIALISED;
-		control.setUi(this);
-	}
-	
-	private String input(String prompt) {
-		System.out.print(prompt);
-		return input.nextLine();
-	}
-		
-	private void output(Object object) {
-		System.out.println(object);
-	}
-			
-	public void setState(UI_STATE state) {
-		this.state = state;
-	}
-	
-	public void run() {
-		output("Borrow Book Use Case UI\n");
-		
-		while (true) {
-			switch (state) {
+    public static enum UI_STATE { INITIALISED, READY, RESTRICTED, SCANNING, IDENTIFIED, FINALISING, COMPLETED, CANCELLED };
+
+    private BorrowBookControl control;
+    private Scanner input;
+    private UI_STATE state;
+
+    public BorrowBookUI(BorrowBookControl control) {
+        this.control = control;
+        input = new Scanner(System.in);
+        state = UI_STATE.INITIALISED;
+        control.setUi(this);
+    }
+
+    private String input(String prompt) {
+        System.out.print(prompt);
+        return input.nextLine();
+    }
+        
+    private void output(Object object) {
+        System.out.println(object);
+    }
+
+    public void setState(UI_STATE state) {
+        this.state = state;
+    }
+
+    public void run() {
+        output("Borrow Book Use Case UI\n");
+
+        while (true) {
+            switch (state) {
                 case CANCELLED:
                     output("Borrowing Cancelled");
                     return;
-                    
+
                 case READY:
                     String memStr = input("Swipe member card (press <enter> to cancel): ");
                     if (memStr.length() == 0) {
@@ -52,12 +52,12 @@ public class BorrowBookUI {
                         output("Invalid Member Id");
                     }
                     break;
-                    
+
                 case RESTRICTED:
                     input("Press <any key> to cancel");
                     control.cancel();
                     break;
-                    
+
                 case SCANNING:
                     String bookStringInput = input("Scan Book (<enter> completes): ");
                     if (bookStringInput.length() == 0) {
@@ -72,7 +72,7 @@ public class BorrowBookUI {
                         output("Invalid Book Id");
                     } 
                     break;
-                    
+
                 case FINALISING:
                     String ans = input("Commit loans? (Y/N): ");
                     if (ans.toUpperCase().equals("N")) {
@@ -82,20 +82,20 @@ public class BorrowBookUI {
                         input("Press <any key> to complete ");
                     }
                     break;
-                    
+
                 case COMPLETED:
                     output("Borrowing Completed");
                     return;
-        
+
                 default:
                     output("Unhandled state");
-                    throw new RuntimeException("BorrowBookUI : unhandled state :" + state);			
+                    throw new RuntimeException("BorrowBookUI : unhandled state :" + state);            
             }
         }
-	}
+    }
 
-	public void display(Object object) {
-		output(object);		
-	}
+    public void display(Object object) {
+        output(object);        
+    }
 
 }
