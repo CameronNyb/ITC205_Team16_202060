0f88c25ab9865c050538e94e1fc126474d735548
samuli64 <samuli64@users.noreply.github.com>
2020-08-18 00:15:19 +1000

    Code Style 2 (method names) in Main

--- src/library/Main.java
+++ src/library/Main.java
@@ -26,7 +26,7 @@ public class Main {
 	private static SimpleDateFormat simpleDateFormat;
 	
 	
-	private static String Get_menu() {
+	private static String getMenu() {
 		StringBuilder stringBuilder = new StringBuilder();
 		
 		stringBuilder.append("\nLibrary Main Menu\n\n")
@@ -67,7 +67,7 @@ public class Main {
 				output(book);
 			}
 						
-			menu = Get_menu();
+			menu = getMenu();
 			
 			boolean exit = false;
 			
@@ -79,43 +79,43 @@ public class Main {
 				switch (c.toUpperCase()) {
 				
 				case "M": 
-					ADD_MEMBER();
+					addMember();
 					break;
 					
 				case "LM": 
-					LIST_MEMBERS();
+					listMembers();
 					break;
 					
 				case "B": 
-					ADD_BOOK();
+					addBook();
 					break;
 					
 				case "LB": 
-					LIST_BOOKS();
+					listBooks();
 					break;
 					
 				case "FB": 
-					FIX_BOOKS();
+					fixBooks();
 					break;
 					
 				case "L": 
-					BORROW_BOOK();
+					borrowBook();
 					break;
 					
 				case "R": 
-					RETURN_BOOK();
+					returnBook();
 					break;
 					
 				case "LL": 
-					LIST_CURRENT_LOANS();
+					listCurrentLoans();
 					break;
 					
 				case "P": 
-					PAY_FINES();
+					payFines();
 					break;
 					
 				case "T": 
-					INCREMENT_DATE();
+					incrementDate();
 					break;
 					
 				case "Q": 
@@ -136,12 +136,12 @@ public class Main {
 	}	
 
 	
-	private static void PAY_FINES() {
+	private static void payFines() {
 		new PayFineUI(new pAY_fINE_cONTROL()).RuN();		
 	}
 
 
-	private static void LIST_CURRENT_LOANS() {
+	private static void listCurrentLoans() {
 		output("");
 		for (Loan loan : library.listCurrentLoans()) {
 			output(loan + "\n");
@@ -150,7 +150,7 @@ public class Main {
 
 
 
-	private static void LIST_BOOKS() {
+	private static void listBooks() {
 		output("");
 		for (Book book : library.listBooks()) {
 			output(book + "\n");
@@ -159,7 +159,7 @@ public class Main {
 
 
 
-	private static void LIST_MEMBERS() {
+	private static void listMembers() {
 		output("");
 		for (Member member : library.listMembers()) {
 			output(member + "\n");
@@ -168,22 +168,22 @@ public class Main {
 
 
 
-	private static void BORROW_BOOK() {
+	private static void borrowBook() {
 		new BorrowBookUI(new bORROW_bOOK_cONTROL()).RuN();		
 	}
 
 
-	private static void RETURN_BOOK() {
+	private static void returnBook() {
 		new ReturnBookUI(new rETURN_bOOK_cONTROL()).RuN();		
 	}
 
 
-	private static void FIX_BOOKS() {
+	private static void fixBooks() {
 		new FixBookUI(new fIX_bOOK_cONTROL()).RuN();		
 	}
 
 
-	private static void INCREMENT_DATE() {
+	private static void incrementDate() {
 		try {
 			int days = Integer.valueOf(input("Enter number of days: ")).intValue();
 			calendar.incrementDate(days);
@@ -196,7 +196,7 @@ public class Main {
 	}
 
 
-	private static void ADD_BOOK() {
+	private static void addBook() {
 		
 		String author = input("Enter author: ");
 		String title  = input("Enter title: ");
@@ -207,7 +207,7 @@ public class Main {
 	}
 
 	
-	private static void ADD_MEMBER() {
+	private static void addMember() {
 		try {
 			String lastName = input("Enter last name: ");
 			String firstName  = input("Enter first name: ");
